0aff13aef5fad8d83938cf9ab8357da8dcab90af
AleinikauArtsiom <artem27082003@mail.ru>
2023-11-18 04:01:04 +0300

    less122

--- src/Lessons12/Hm1.java
+++ src/Lessons12/Hm1.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class Hm1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-       // String line = scan.nextLine();
+       /* String line = scan.nextLine(); */
         Pattern pattern = Pattern.compile("[A-Z]{2,6}");
         Matcher matcher = pattern.matcher(scan.nextLine());
         while (matcher.find()){
